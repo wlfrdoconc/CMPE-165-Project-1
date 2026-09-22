8b6b04fb8b832ce55c5cbb677a5bb22701b45ec6
Joseph Centeno <centenojoseph9@gmail.com>
2026-09-21 17:44:15 -0700

    defined the database path to promote DRY code. Implemented create_semester_plan() and add_course_to_plan().

--- backend/database/database_operations.py
+++ backend/database/database_operations.py
@@ -1,8 +1,11 @@
+import os
 import sqlite3
+
+database_path = os.path.join(os.path.dirname(__file__), "sem_diff_predi.db")
 
 # Adds a student to the database. Parameters are not passed in directly to prevent SQL injections.
 def add_student(major: str, student_year: int, student_type: str, learning_style: str) -> int:
-    connection = sqlite3.connect("sem_diff_predi.db")
+    connection = sqlite3.connect(database_path)
     cursor = connection.cursor()
 
     cursor.execute("""
@@ -19,7 +22,7 @@ def add_student(major: str, student_year: int, student_type: str, learning_style
 
 # Adds a course to the database.
 def add_course(course_code: str, title: str, course_description: str, units: int, course_level: str, subject_area: str, course_type: str, complexity_score: int) -> int:
-    connection = sqlite3.connect("sem_diff_predi.db")
+    connection = sqlite3.connect(database_path)
     cursor = connection.cursor() 
 
     if get_course_id_by_code(course_code) is not None:
@@ -40,7 +43,7 @@ def add_course(course_code: str, title: str, course_description: str, units: int
 
 # Grabs the course_id based on a course_code. Used as a helper function in completed_course()
 def get_course_id_by_code(course_code: str) -> int:
-    connection = sqlite3.connect("sem_diff_predi.db")
+    connection = sqlite3.connect(database_path)
     cursor = connection.cursor()
 
     res = cursor.execute("""
@@ -51,7 +54,6 @@ def get_course_id_by_code(course_code: str) -> int:
     connection.close()
         
     if res is None:
-        print('course_id does not exist.')
         return None
 
     return res[0]
@@ -64,7 +66,7 @@ def add_completed_course(student_id: int, course_code: str, grade: str) -> bool:
         print('course_id does not exist.')
         return None
 
-    connection = sqlite3.connect("sem_diff_predi.db")
+    connection = sqlite3.connect(database_path)
     cursor = connection.cursor()
 
     cursor.execute("""
@@ -79,7 +81,7 @@ def add_completed_course(student_id: int, course_code: str, grade: str) -> bool:
 
 # Returns a set of classes that a student has already completed.
 def get_completed_course_ids(student_id: int) -> set:
-    connection = sqlite3.connect("sem_diff_predi.db")
+    connection = sqlite3.connect(database_path)
     cursor = connection.cursor()
 
     completed_courses_ids = cursor.execute("""
@@ -98,7 +100,7 @@ def get_completed_course_ids(student_id: int) -> set:
 
 # Compares the prerequisites of a course to the completed courses of a student.
 def check_prerequisites(course_id: int, student_id: int) -> set:
-    connection = sqlite3.connect("sem_diff_predi.db")
+    connection = sqlite3.connect(database_path)
     cursor = connection.cursor()
 
     course_prerequisites = cursor.execute("""
@@ -122,8 +124,8 @@ def check_prerequisites(course_id: int, student_id: int) -> set:
         return missing_prerequisites
 
 # Displays classes the student can take. 
-def available_classes(student_id: int):
-    connection = sqlite3.connect("sem_diff_predi.db")
+def get_available_courses(student_id: int) -> list:
+    connection = sqlite3.connect(database_path)
     cursor = connection.cursor()
 
     all_courses = cursor.execute("SELECT course_id FROM course").fetchall()
@@ -137,3 +139,45 @@ def available_classes(student_id: int):
             available.append(course[0])
 
     return available
+
+# Calculate 
+
+# Allow the student to create a semester plan. 
+def create_semester_plan(student_id: int, semester: str, semester_year: int) -> int:
+    connection = sqlite3.connect(database_path)
+    cursor = connection.cursor()
+
+    cursor.execute("""
+        INSERT INTO semester_plan (student_id, semester, semester_year)
+        VALUES (?, ?, ?)
+    """, (student_id, semester, semester_year))
+
+    plan_id = cursor.lastrowid
+
+    connection.commit()
+    connection.close()
+
+    return plan_id
+
+# Allow the student to add courses to an existing semester plan
+def add_course_to_plan(plan_id: int, course_id: int, prior_experience) -> bool:
+    connection = sqlite3.connect(database_path)
+    cursor = connection.cursor()
+
+    check_plan_id_exists = cursor.execute("""
+        SELECT plan_id FROM semester_plan
+        WHERE plan_id = ?
+    """ (plan_id, ))
+
+    if not check_plan_id_exists:
+        return False
+
+    cursor.execute("""
+        INSERT INTO plan_course (plan_id, course_id, prior_experience)
+        VALUES (?, ?, ?)
+    """, (plan_id, course_id, prior_experience))
+
+    connection.commit()
+    connection.close()
+
+    return True
